3286b81589c61c2ef47c6dc875192bee66677865
AppSolves <appsolves@gmail.com>
2026-02-22 19:31:12 +0100

    Add Windows runner support for high DPI and console output
    - Introduced a manifest file to enable PerMonitorV2 DPI awareness and support for Windows 10 and 11.
    - Implemented console creation and output redirection in utils.cpp to facilitate debugging.
    - Added functions to convert command line arguments from UTF-16 to UTF-8.
    - Created a Win32Window class to manage window creation, theming, and DPI scaling.
    - Implemented dark mode support by checking system theme preferences.
    - Added necessary header files for utility functions and window management.

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <event_log/event_log_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  EventLogPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("EventLogPlugin"));
+}
